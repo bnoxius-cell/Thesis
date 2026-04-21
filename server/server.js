aa3f3aa498062577824ac02f15aab1bb07e8da00
bnoxius-cell <idevadrian@gmail.com>
2026-04-21 20:14:55 +0800

    init db connection

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import connectDB from '../server/config/db.js';
+import 'dotenv/config'
+
+// init app
+const app = express();
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
+app.use(cookieParser());
+
+// declare port and connect to db
+const PORT = process.env.PORT || 5000;
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
