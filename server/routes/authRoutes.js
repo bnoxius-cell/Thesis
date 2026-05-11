888a68f95c33235c0b16278770dba70af4a1ed3a
bnoxius-cell <idevadrian@gmail.com>
2026-05-11 15:50:27 +0800

    implemented google auth

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { login, logout, register, sendVerifyOtp, verifyEmail, isAuthenticated, sendResetPasswordOtp, resetPassword} from '../controllers/authController.js'
+import { login, logout, register, sendVerifyOtp, verifyEmail, isAuthenticated, sendResetPasswordOtp, resetPassword, googleLogin} from '../controllers/authController.js'
 import userAuth from '../middleware/userAuth.js'
 
 const authRouter = express.Router();
@@ -12,5 +12,6 @@ authRouter.post('/verify-email', userAuth, verifyEmail);
 authRouter.post('/is-authenticated', userAuth, isAuthenticated);
 authRouter.post('/send-reset-otp', sendResetPasswordOtp);
 authRouter.post('/reset-password', resetPassword);
+authRouter.post('/google', googleLogin);
 
 export default authRouter;
